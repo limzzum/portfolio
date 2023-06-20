8976ebecec843c8fb296e1c7ac06c9ded086853a
limzzum <chfhddl1234@naver.com>
2023-06-21 00:40:43 +0900

    feat : 블로그 게시물 링크 추가

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,64 @@
-
-$(document).ready(function() {
-    general_utils();
-    blog_posts();
-})
-
+$(document).ready(function () {
+  general_utils();
+  blog_posts();
+});
 
 function general_utils() {
-    // smooth scrolling for nav links
-    $('.head-menu-wrap a').smoothScroll();
-    $('.extra-link a').smoothScroll();
-    $('.profile-pic-link').smoothScroll();
+  // smooth scrolling for nav links
+  $(".head-menu-wrap a").smoothScroll();
+  $(".extra-link a").smoothScroll();
+  $(".profile-pic-link").smoothScroll();
 
-    $('.skillbar').each(function(){
-		$(this).find('.skillbar-bar').animate({
-			width: $(this).attr('data-percent')
-		}, 1000);
-	});
+  $(".skillbar").each(function () {
+    $(this)
+      .find(".skillbar-bar")
+      .animate(
+        {
+          width: $(this).attr("data-percent"),
+        },
+        1000
+      );
+  });
 }
 
 function blog_posts() {
+  // keeping it static, can be fetched from a blog dynamically as well
+  let posts = [
+    {
+      url: "https://zammanboss.tistory.com/4",
+      title: "[SOLID] 객체 지향 5가지 원칙",
+    },
+    {
+      url: "https://zammanboss.tistory.com/3",
+      title:
+        "[알고리즘 #1] 유클리드 호제법 & 확장 유클리드 호제법 쉽게 이해하기",
+    },
+    {
+      url: "https://zammanboss.tistory.com/8",
+      title: "동형암호",
+    },
+    {
+      url: "https://zammanboss.tistory.com/14",
+      title: "메타버스",
+    },
+    {
+      url: "https://zammanboss.tistory.com/23",
+      title: "계단 오르기 Java 자바",
+    },
+  ];
 
-    // keeping it static, can be fetched from a blog dynamically as well
-    let posts = [
-        {
-            url: 'https://www.nagekar.com/2017/02/trip-to-bramhatal-uttarakhand.html',
-            title: 'Trek To Bramhatal (Uttarakhand)',
-        },
-        {
-            url: 'https://www.nagekar.com/2017/08/privacy.html',
-            title: 'Privacy - How I Converted',
-        },
-        {
-            url: 'https://www.nagekar.com/2018/01/jagriti-yatra.html',
-            title: 'Jagriti Yatra 2017',
-        },
-        {
-            url: 'https://www.nagekar.com/2017/08/private-cloud-part-2.html',
-            title: 'Private Cloud Part 2 | Encrypted Storage With NextCloud',
-        },
-        {
-            url: 'https://www.nagekar.com/2018/07/eli5-how-https-works.html',
-            title: 'ELI5 - How HTTPS Works',
-        },
-    ];
+  let post_html = [];
 
-    let post_html = [];
+  for (let post of posts) {
+    let tags;
 
-    for(let post of posts) {
+    if (post.tags) {
+      tags = post.tags.map((tag) => {
+        return `<a href="https://www.nagekar.com/tags#${tag}">${tag}</a>`;
+      });
+    }
 
-        let tags;
-        
-        if(post.tags) {
-            tags = post.tags.map(tag => {
-                return `<a href="https://www.nagekar.com/tags#${tag}">${tag}</a>`
-            })
-        }
-
-        let post_template = `
+    let post_template = `
         <div class="blog-post" onclick="blog_link_click('${post.url}');">
 
             <div class="blog-link">
@@ -71,11 +73,11 @@ function blog_posts() {
         </div>
         `;
 
-        post_html.push(post_template);
-    }
+    post_html.push(post_template);
+  }
 
-    // for the more posts link
-    let post_template = `
+  // for the more posts link
+  let post_template = `
     <div class="blog-post more-blogs" onclick="blog_link_click('https://www.nagekar.com');">
 
         <div class="blog-link">
@@ -90,12 +92,11 @@ function blog_posts() {
     </div>
     `;
 
-    post_html.push(post_template);
+  post_html.push(post_template);
 
-    $('#rss-feeds').html(post_html);
-
+  $("#rss-feeds").html(post_html);
 }
 
 function blog_link_click(url) {
-    window.location = url;
+  window.open(url, "_blank");
 }
