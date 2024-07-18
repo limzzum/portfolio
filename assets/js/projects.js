a872156228ac1f6fe1790794b02eadb398843182
limzzum <chfhddl1234@naver.com>
2024-07-18 14:47:17 +0900

    :sparkles: feat : 프로젝트 추가

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -13,7 +13,7 @@ let render_projects = (slug) => {
       image: "assets/images/말해Vr.png",
       link: "https://github.com/limzzum/SayVr",
       title: "영어 회화 학습 웹 서비스",
-      demo: true,
+      demo: "",
       technologies: ["SpringBoot", "Java", "JPA", "RabbitMq"],
       description:
         "영어 학습을 위한 단어장, 스터디, 쉐도잉, 매칭 게임, 잔디 관리 등의 서비스를 제공해준다.",
@@ -54,18 +54,39 @@ let render_projects = (slug) => {
       title: "토론 승패 판단 서비스",
       demo: false,
       technologies: ["SpringBoot", "Java", "Web Socket"],
-      description: "Web RTC를 이용한 웹 토론 서비스",
-      categories: ["web"],
+      description:
+        "WebRTC를 이용한 웹 토론 서비스로, 욕설 인식과 관전자 투표 등을 통해 승패가 결정된다.",
+      categories: ["featured", "web"],
     },
     {
       image: "assets/images/Nova.png",
       link: "https://github.com/NOVA-App/NOVA",
       title: "노후 자산 시뮬레이션",
       demo: false,
-      technologies: ["SpringBoot", "Java", "JPA", "Spring Security"],
+      technologies: ["SpringBoot", "JPA", "Spring Security"],
       description:
         "주식, 적금 등 27세부터 60세까지 자산을 운용해보며 노후에 어느정도의 생활을 할 수 있을지 시뮬레이션을 통해 예측해볼 수 있다.",
       categories: ["featured", "app"],
+    },
+    {
+      image: "assets/images/반쪽.png",
+      link: "https://github.com/limzzum/banjjog.git",
+      title: "내 반쪽 연인과의 궁합 확인",
+      demo: false,
+      technologies: ["React", "Typescript", "Wep/App"],
+      description:
+        "문답을 통해 연인과 나의 가치관이 얼마나 일치하는지, 서로에 대해 얼마나 알고 있는지 확인해 볼 수 있다.",
+      categories: ["app", "web"],
+    },
+    {
+      image: "assets/images/potato.png",
+      link: "https://github.com/potathon/team1-backend",
+      title: "가상 면접 시뮬레이션",
+      demo: false,
+      technologies: ["SpringBoot", "JPA", "OpenAI", "해커톤"],
+      description:
+        "개발자에게 필요한 지식들을 묻는 가상면접을 통해 내 답변을 확인&기록 하고, 다른 사람들과 공유할 수 있다.",
+      categories: ["web"],
     },
   ];
 
